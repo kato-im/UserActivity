52c845fea34e636c21cd4258ced948c0edb70482
Shaladonov Vyacheslav <vyacheslav.shaladonov@touchin.ru>
2014-07-07 03:32:19 +0300

    Initial Commit

---
+++ UserActivity/UserActivity.h
@@ -0,0 +1,13 @@
+//
+//  UserActivity.h
+//  UserActivity
+//
+//  Created by Krypt on 07.07.14.
+//  Copyright (c) 2014 kato. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserActivity : NSObject
+
+@end
---
+++ UserActivity/UserActivity.m
@@ -0,0 +1,13 @@
+//
+//  UserActivity.m
+//  UserActivity
+//
+//  Created by Krypt on 07.07.14.
+//  Copyright (c) 2014 kato. All rights reserved.
+//
+
+#import "UserActivity.h"
+
+@implementation UserActivity
+
+@end
